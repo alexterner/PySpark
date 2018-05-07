9189523f56a32cb21fb5561be32c4c98eaf130f4
Alex Terner <AlexTerner@gmail.com>
2018-05-07 08:56:54 +0300

    adding statefull word count

---
+++ statefullWordCount/statefullWordCount.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+
+import sys
+
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: statefulwordCount.py <hostname> <port>", file=sys.stderr)
+        sys.exit(-1)
+    sc = SparkContext(appName="StatefukWordCount")
+    ssc = StreamingContext(sc, 1)
+    ssc.checkpoint("/tmp/checkpoint")
+
+    # RDD with initial state (key, value) pairs
+    initialStateRDD = sc.parallelize([(u'hello', 1), (u'world', 1)])
+
+    def updateFunc(new_values, last_sum):
+        return sum(new_values) + (last_sum or 0)
+
+    lines = ssc.socketTextStream(sys.argv[1], int(sys.argv[2]))
+
+    running_counts = lines.flatMap(lambda line: line.split(" "))\
+                          .map(lambda word: (word, 1))\
+                          .updateStateByKey(updateFunc, initialRDD=initialStateRDD)
+
+    running_counts.pprint()
+
+    ssc.start()
+    ssc.awaitTermination()
